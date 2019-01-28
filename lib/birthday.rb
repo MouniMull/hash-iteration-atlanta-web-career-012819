9e18f101b8d68e80256073ee01ebf53505285dad
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-28 06:23:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do {|name, age| puts ""
-
-
+  birthday_kids.each do {|name, age| puts "Happy Birthday #{name}! You are now #{age} years old!"}
+  end 
 end
 
 
